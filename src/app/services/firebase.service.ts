cd38d81ca6c78f9add189ed9ff16132f0f4e15b6
faber.munoz <jhonfaber0803@gmail.com>
2023-05-23 17:14:28 -0500

    ajustes

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Auth,signInWithEmailAndPassword,signOut,createUserWithEmailAndPassword, getAuth} from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, getAuth } from '@angular/fire/auth';
 import { Firestore, doc, setDoc, collection, addDoc, collectionData, getDoc } from '@angular/fire/firestore';
 import { Post } from '../components/post/post.model';
 import { Observable } from 'rxjs';
@@ -8,30 +8,30 @@ import { Observable } from 'rxjs';
 })
 export class FirebaseService {
 
-  authf=getAuth()
-  constructor(private auth:Auth,private firestore:Firestore) { }
-  login(objeto:any){
-    let email=objeto["email"]
-    let password=objeto["password"]
-    return signInWithEmailAndPassword(this.auth,email,password)
+  authf = getAuth()
+  constructor(private auth: Auth, private firestore: Firestore) { }
+  login(objeto: any) {
+    let email = objeto["email"]
+    let password = objeto["password"]
+    return signInWithEmailAndPassword(this.auth, email, password)
   }
-  userObserver(){
-    let usuario=this.auth.currentUser
+  userObserver() {
+    let usuario = this.auth.currentUser
     return usuario
   }
-  cerrarSesion(){
+  cerrarSesion() {
     return signOut(this.auth)
   }
-  signUp(objeto:any){
-    let email=objeto["email"]
-    let password=objeto["password"]
-    return createUserWithEmailAndPassword(this.auth,email,password)
+  signUp(objeto: any) {
+    let email = objeto["email"]
+    let password = objeto["password"]
+    return createUserWithEmailAndPassword(this.auth, email, password)
   }
 
-  addPersona(usuario:any,id:string){
-    usuario.estado=true;
-    const usuarioRef=doc(this.firestore,"usuarios",id)
-    return setDoc(usuarioRef,usuario)
+  addPersona(usuario: any, id: string) {
+    usuario.estado = true;
+    const usuarioRef = doc(this.firestore, "usuarios", id)
+    return setDoc(usuarioRef, usuario)
   }
 
   async getPersona(id: string) {
@@ -41,7 +41,7 @@ export class FirebaseService {
       if (docSnap.exists()) {
         return docSnap.data();
       } else {
-        console.log("User does not exist");
+        console.log("El usuario no existe");
       }
     } catch (error) {
       console.log(error);
@@ -53,7 +53,7 @@ export class FirebaseService {
     const user = this.userObserver();
     if (user != null) {
       return this.getPersona(user.uid);
-    } 
+    }
     return undefined;
   }
 
@@ -62,17 +62,15 @@ export class FirebaseService {
     if (user) {
       post.autor = user['firstName'] + ' ' + user['lastName'];
       post.document = 'document.pdf'
-      const postId = collection(this.firestore, 'places');
+      const postId = collection(this.firestore, 'posts');
       return addDoc(postId, post);
     } else {
       throw new Error('No se pudo obtener el usuario activo.');
     }
   }
-  
+
   getPosts(): Observable<Post[]> {
-    
-    
-    const postId = collection(this.firestore, 'places');
-    return collectionData(postId, {idField: 'id'}) as Observable<Post[]>;
+    const postId = collection(this.firestore, 'posts');
+    return collectionData(postId, { idField: 'id' }) as Observable<Post[]>;
   }
 }
